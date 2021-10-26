5bcda5b949ae6b3d00dda9b84cdfe5bce71a8f36
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-10-25 23:10:41 -0400

    compare two arrays

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Asserion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.join(), ["Lighthouse", "Labs"].join());
