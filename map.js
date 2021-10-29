29f1c9d434333c19d04b71de50bf7581826d1154
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-10-28 22:54:41 -0400

    using Map funct

---
+++ map.js
@@ -0,0 +1,33 @@
+function eqArrays(arr1, arr2) {
+  return arr1[0] === arr2[0] && arr1[1] === arr2[1] && arr1[2] === arr2[2];
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${arr1}  === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Asserion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const map = function (words, callback) {
+  const results = [];
+  for (let item of words) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const words = ["ground", "control", "to", "major", "tom"];
+
+//const results1 = map(words, (word) => word[0]);
+//console.log(results1);
+
+assertArraysEqual(
+  map(words, (word) => word[0]),
+  ["g", "c", "t", "m", "t"]
+);
+
+assertArraysEqual(
+  map(words, (word) => word.length),
+  [6, 7, 2, 5, 3]
+);
