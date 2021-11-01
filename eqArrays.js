808a1d2456af7cec9f4b43060ce1a00cbea44527
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-01 14:57:00 -0400

    updated with modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,6 @@
 /* eslint-disable func-style */
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Asserion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 function eqArrays(arr1, arr2) {
   return arr1[0] === arr2[0] && arr1[1] === arr2[1] && arr1[2] === arr2[2];
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Asserion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
