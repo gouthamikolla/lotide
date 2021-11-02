08afb91a30c1b35280a41e1f5ee809f6e9489419
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-01 20:04:57 -0400

    Testcases refactored using Mocha/chai

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
+    //console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
+    return;
   } else {
     console.log(`🛑🛑🛑 Asserion Failed: ${actual} !== ${expected}`);
   }
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-undef */
 
-const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+describe("#eqArraysTest", () => {
+  it("returns true for [1, 2, 3] = [1,2,3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3],[1,2,3]), true);
+  });
+  it("returns false for [1, 2, 3] = [3, 2, 1]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3],[3,2,1]), false);
+  });
+  it("returns true for ['1', '2', '3'] = ['1', '2', '3']", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("returns false for ['1','2','3'] = ['1','2',3]", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+});
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
